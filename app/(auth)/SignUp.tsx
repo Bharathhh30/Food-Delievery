ef42ec3b213eb535ee60722f1250d917950d6317
Bharathhh30 <vnsb30@gmail.com>
2025-07-05 11:26:23 +0530

    coded auth screens

--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -1,15 +1,65 @@
-import { router } from 'expo-router'
-import React from 'react'
-import { Button, Text, View } from 'react-native'
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignUp = () => {
-  return (
-    <View>
-      <Text>SignUp</Text>
-            <Button title='Sign In' onPress={() => router.push("/SignIn")}></Button>
-      
-    </View>
-  )
-}
+  const [isSubmitting,setIsSubmitting] = useState(false)
+  const [form,setForm] = useState({name:"",email : "",password : ''})
 
-export default SignUp
+  const submit = async() => {
+    if (!form.name|| !form.email || !form.password) return Alert.alert('Error','Please enter valid Email address and password')
+    setIsSubmitting(true)
+
+    try{
+      // call appwrite sign up function
+      Alert.alert('Success', 'User Signed up successfully')
+      router.replace('/')
+    }catch(error){
+      Alert.alert('Error')
+    }finally{
+      setIsSubmitting(false)
+    }
+  }
+  return (
+    <View className="gap-10 bg-white  rounded-lg p-5 mt-5 ">
+      <CustomInput
+        placeholder="Enter your name"
+        value={form.name}
+        onChangeText={(text) => setForm((prev)=>({...prev,name:text}))}
+        label="Email"
+      />
+      <CustomInput
+        placeholder="Enter your email"
+        value={form.email}
+        onChangeText={(text) => setForm((prev)=>({...prev,email:text}))}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={form.password}
+        onChangeText={(text) => setForm((prev)=>({...prev,password:text}))}
+        label="Password"
+        secureTextEntry = {true} //password should not be appearing bro
+      />
+      <CustomButton 
+        title="Sign Up"
+        isLoading = {isSubmitting}
+        onPress={submit}
+      />
+
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Already have an account ?
+        </Text>
+        <Link href={"/SignIn"} className="base-bold text-primary"> 
+          Sign In
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+export default SignUp;
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform : "com.jsm.foodordering",
+    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId : "6868b46d002604c0b6aa",
+    userCollectionId : "6868b4940038085a1a50"
+}
